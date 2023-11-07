29d17b662051e4f5dbc5c5202b0d429db14ed957
ellenmcintyre123 <g00389482@atu.ie>
2023-11-07 16:25:33 +0000

    exam part 3

---
+++ src/main/java/ie/atu/requesthandlings/RequestHandlingsApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.requesthandlings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestHandlingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestHandlingsApplication.class, args);
+    }
+
+}
